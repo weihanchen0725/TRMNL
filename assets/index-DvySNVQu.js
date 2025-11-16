acf9818cd675e6047f1875d5b35c59147a16ff69
WeiHanChen <weihanchen0725@gmail.com>
2025-11-16 00:00:33 -0800

    Updates

--- assets/index-DvySNVQu.js
+++
@@ -1,5 +0,0 @@
-(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const s of t.addedNodes)s.tagName==="LINK"&&s.rel==="modulepreload"&&i(s)}).observe(document,{childList:!0,subtree:!0});function o(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function i(e){if(e.ep)return;e.ep=!0;const t=o(e);fetch(e.href,t)}})();const c="/TRMNL.WIFIQRCODE/assets/QR_WiFi-q9weLz3n.png";function u(n){let r=0;const o=i=>{r=i,n.innerHTML=`count is ${r}`};n.addEventListener("click",()=>o(r+1)),o(0)}document.querySelector("#app").innerHTML=`
-  <div>
-    <img src="${c}" alt="WiFi QR Code" />
-  </div>
-`;u(document.querySelector("#counter"));
