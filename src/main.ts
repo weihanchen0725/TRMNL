33ef41b7eca0607301fce8a647a45f885089cbcf
WeiHanChen <weihanchen0725@gmail.com>
2025-11-15 17:45:27 -0800

    feat: simplify main layout and enhance e-ink display styles

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,9 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
 import qrWifiImage from './assets/images/QR_WiFi.png'
 import { setupCounter } from './counter.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
     <img src="${qrWifiImage}" alt="WiFi QR Code" />
   </div>
 `
